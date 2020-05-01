dd731c484a391dc6ced544458c2a7677b7879e16
yfuku <fukumoto@gmail.com>
2020-05-01 20:15:34 +0900

    trackball

--- keyboards/bat43/keymaps/trackball/keymap.c
+++ keyboards/bat43/keymaps/trackball/keymap.c
@@ -15,6 +15,9 @@
  */
 #include QMK_KEYBOARD_H
 
+#include "paw3204.h"
+#include "pointing_device.h"
+
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
@@ -48,3 +51,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______,          _______, _______, _______
    ),
 };
+
+void matrix_init_user(void) {
+    init_paw3204();
+}
+
+report_mouse_t mouse_rep;
+
+void keyboard_post_init_user() {
+    debug_enable = true;
+    debug_mouse = true;
+}
+
+void matrix_scan_user(void) {
+    static int  cnt;
+    static bool paw_ready;
+    if (cnt++ % 50 == 0) {
+        uint8_t pid = read_pid_paw3204();
+        if (pid == 0x30) {
+            dprint("paw3204 OK\n");
+            paw_ready = true;
+        } else {
+            dprintf("paw3204 NG:%d\n", pid);
+            paw_ready = false;
+        }
+    }
+
+    if (paw_ready) {
+        uint8_t stat;
+        int8_t x, y;
+        //int8_t r_x, r_y;
+        //int8_t kakudo = 45;
+
+        read_paw3204(&stat, &x, &y);
+
+        //r_x = x * cos(kakudo) + y * sin(kakudo);
+        //r_y = -x * sin(kakudo) + y * cos(kakudo);
+
+        mouse_rep.buttons = 0;
+        mouse_rep.h       = 0;
+        mouse_rep.v       = 0;
+        mouse_rep.x       = y;
+        mouse_rep.y       = -x;
+
+        if (cnt % 10 == 0) {
+            dprintf("stat:%3d x:%4d y:%4d\n", stat, mouse_rep.x, mouse_rep.y);
+        }
+
+        if (stat & 0x80) {
+            pointing_device_set_report(mouse_rep);
+        }
+    }
+}

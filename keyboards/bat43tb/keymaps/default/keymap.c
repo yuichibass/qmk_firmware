61c1f4711fbcdf7c5655c2d5267ba9b91ac61a84
yfuku <fukumoto@gmail.com>
2020-09-23 23:15:21 +0900

    多少はみやすく・・

--- keyboards/bat43tb/keymaps/default/keymap.c
+++ keyboards/bat43tb/keymaps/default/keymap.c
@@ -43,22 +43,22 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-	KC_ESC,	KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	KC_Y,	KC_U,	KC_I,	KC_O,	KC_P,	KC_MINS,
-	KC_TAB,	KC_A,	SGUI_T(KC_S),	LALT_T(KC_D),	LGUI_T(KC_F),	 LCTL_T(KC_G),    KC_NO,    RCTL_T(KC_H),	RGUI_T(KC_J),	RALT_T(KC_K),	SGUI_T(KC_L),	KC_SCLN,	KC_ESC,
-	KC_LEFT,	KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,   MBTN1, MBTN3,   KC_N,	KC_M,	KC_COMM,	KC_DOT,	KC_SLSH,	KC_RIGHT,
-				KC_DOWN, LSFT_T(KC_LANG2),	LT(1, KC_SPC),    MBTN2,    LT(2, KC_ENT),	LSFT_T(KC_LANG1),	KC_UP
+  KC_ESC,   KC_Q,  KC_W,         KC_E,         KC_R,         KC_T,                         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,     KC_MINS,
+  KC_TAB,   KC_A,  SGUI_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), LCTL_T(KC_G),     KC_NO,      RCTL_T(KC_H), RGUI_T(KC_J), RALT_T(KC_K), SGUI_T(KC_L), KC_SCLN,  KC_ESC,
+  KC_LEFT,  KC_Z,  KC_X,         KC_C,         KC_V,         KC_B,          MBTN1, MBTN3,  KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,  KC_RIGHT,
+                                   MBTN1, LSFT_T(KC_LANG2), LT(1, KC_SPC),     MBTN2,      LT(2, KC_ENT), LSFT_T(KC_LANG1), MBTN2
     ),
     [_LOWER] = LAYOUT(
-	KC_TRNS,	KC_NO,	KC_NO, KC_NO,	KC_NO,	KC_NO,		KC_NO,	KC_EQL,	KC_PLUS,	KC_ASTR,	KC_PERC,	KC_TRNS,
-	KC_TRNS,	KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_NO, KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_TRNS,
-	KC_TRNS,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	KC_NO,	       KC_NO, KC_NO,	     KC_NO,	KC_NO,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,
-				RESET,	KC_TRNS,	KC_NO,     KC_NO,  		KC_DEL,	KC_TRNS,	KC_TRNS			
+  KC_TRNS,  KC_NO,  KC_NO, KC_NO,  KC_NO,  KC_NO,    KC_NO,  KC_EQL,  KC_PLUS,  KC_ASTR,  KC_PERC,  KC_TRNS,
+  KC_TRNS,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_NO, KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_TRNS,
+  KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,         KC_NO, KC_NO,       KC_NO,  KC_NO,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        RESET,  KC_TRNS,  KC_NO,     KC_NO,      KC_DEL,  KC_TRNS,  KC_TRNS      
     ),
     [_RAISE] = LAYOUT(
-	KC_TRNS,	KC_BSLS,	KC_EXLM,	KC_AMPR,	KC_PIPE,	KC_NO           ,		KC_NO,	KC_EQL,	KC_PLUS,	KC_ASTR,	KC_PERC,	KC_TRNS,
-	KC_TRNS,	KC_HASH,	KC_GRV,	KC_DQUO,	KC_QUOT,	KC_TILD, LCMD(KC_BSPC),	KC_LEFT,	KC_DOWN,	KC_UP,	KC_RGHT,	KC_DLR,	KC_TRNS,
-	KC_TRNS,	KC_NO,	KC_NO,	KC_LCBR,	KC_LBRC,	KC_LPRN, KC_NO, KC_NO,		KC_RPRN,	KC_RBRC,	KC_RCBR,	KC_AT,	KC_CIRC,	KC_TRNS,
-				KC_TRNS,	KC_TRNS,	KC_BSPC, KC_NO,		KC_NO,	KC_TRNS,	RESET
+  KC_TRNS,  KC_BSLS,  KC_EXLM,  KC_AMPR,  KC_PIPE,  KC_NO           ,    KC_NO,  KC_EQL,  KC_PLUS,  KC_ASTR,  KC_PERC,  KC_TRNS,
+  KC_TRNS,  KC_HASH,  KC_GRV,  KC_DQUO,  KC_QUOT,  KC_TILD, LCMD(KC_BSPC),  KC_LEFT,  KC_DOWN,  KC_UP,  KC_RGHT,  KC_DLR,  KC_TRNS,
+  KC_TRNS,  KC_NO,  KC_NO,  KC_LCBR,  KC_LBRC,  KC_LPRN, KC_NO, KC_NO,    KC_RPRN,  KC_RBRC,  KC_RCBR,  KC_AT,  KC_CIRC,  KC_TRNS,
+        KC_TRNS,  KC_TRNS,  KC_BSPC, KC_NO,    KC_NO,  KC_TRNS,  RESET
    ),
 };
 

69b609ffba9f4ba01031170accbe3f7705f3899c
yfuku <fukumoto@gmail.com>
2021-02-12 12:59:17 +0900

    add naginata

---
+++ keyboards/claw44/keymaps/naginata_v13u/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define MASTER_LEFT
+
+#define TAPPING_TERM 180
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TERM_PER_KEY
+
+#define OLED_UPDATE_INTERVAL 100
+#define OLED_TIMEOUT 600000
---
+++ keyboards/claw44/keymaps/naginata_v13u/rules.mk
@@ -0,0 +1,8 @@
+#VIA_ENABLE = yes
+UNICODE_ENABLE = yes
+COMBO_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+
+SRC += naginata_v13u.c
+
+EXTRAFLAGS += -flto
